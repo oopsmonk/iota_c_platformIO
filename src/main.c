c710164f4cea85e1bad3632949e8d760a6ceb562
Sam Chen <sam.chen@iota.org>
2020-10-27 19:39:34 +0800

    :gear: pio esp v2.0.0, idf v4.1

---
+++ src/main.c
@@ -0,0 +1,134 @@
+#include <inttypes.h>
+#include <stdio.h>
+
+#include "esp_event.h"
+#include "esp_log.h"
+#include "esp_spi_flash.h"
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/event_groups.h"
+#include "freertos/task.h"
+#include "nvs_flash.h"
+
+#define APP_WIFI_SSID CONFIG_WIFI_SSID
+#define APP_WIFI_PWD CONFIG_WIFI_PASSWORD
+#define APP_WIFI_RETRY CONFIG_WIFI_RETRY
+
+static const char* TAG = "esp32_main";
+
+// extern const char amazon_cert_pem_start[] asm("_binary_amazon_root_ca1_pem_start");
+extern const uint8_t amazon_cert_pem_start[] asm("_binary_amazon_root_ca1_pem_start");
+
+/* FreeRTOS event group to signal when we are connected*/
+static EventGroupHandle_t s_wifi_event_group;
+
+/* The event group allows multiple bits for each event, but we only care about two events:
+ * - we are connected to the AP with an IP
+ * - we failed to connect after the maximum amount of retries */
+#define WIFI_CONNECTED_BIT BIT0
+#define WIFI_FAIL_BIT BIT1
+
+static int s_retry_num = 0;
+
+static void event_handler(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data) {
+  if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+    esp_wifi_connect();
+  } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
+    if (s_retry_num < APP_WIFI_RETRY) {
+      esp_wifi_connect();
+      s_retry_num++;
+      ESP_LOGI(TAG, "retry to connect to the AP");
+    } else {
+      xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
+    }
+    ESP_LOGI(TAG, "connect to the AP fail");
+  } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+    ip_event_got_ip_t* event = (ip_event_got_ip_t*)event_data;
+    ESP_LOGI(TAG, "got ip:" IPSTR, IP2STR(&event->ip_info.ip));
+    s_retry_num = 0;
+    xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
+  }
+}
+
+static void wifi_conn_init(void) {
+  s_wifi_event_group = xEventGroupCreate();
+
+  ESP_ERROR_CHECK(esp_netif_init());
+
+  ESP_ERROR_CHECK(esp_event_loop_create_default());
+  esp_netif_create_default_wifi_sta();
+
+  wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+  ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+  ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL));
+  ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler, NULL));
+
+  wifi_config_t wifi_config = {
+      .sta =
+          {
+              .ssid = APP_WIFI_SSID,
+              .password = APP_WIFI_PWD,
+              /* Setting a password implies station will connect to all security modes including WEP/WPA.
+               * However these modes are deprecated and not advisable to be used. Incase your Access point
+               * doesn't support WPA2, these mode can be enabled by commenting below line */
+              .threshold.authmode = WIFI_AUTH_WPA2_PSK,
+
+              .pmf_cfg = {.capable = true, .required = false},
+          },
+  };
+  ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+  ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config));
+  ESP_ERROR_CHECK(esp_wifi_start());
+
+  ESP_LOGI(TAG, "wifi_init_sta finished.");
+
+  /* Waiting until either the connection is established (WIFI_CONNECTED_BIT) or connection failed for the maximum
+   * number of re-tries (WIFI_FAIL_BIT). The bits are set by event_handler() (see above) */
+  EventBits_t bits =
+      xEventGroupWaitBits(s_wifi_event_group, WIFI_CONNECTED_BIT | WIFI_FAIL_BIT, pdFALSE, pdFALSE, portMAX_DELAY);
+
+  /* xEventGroupWaitBits() returns the bits before the call returned, hence we can test which event actually
+   * happened. */
+  if (bits & WIFI_CONNECTED_BIT) {
+    ESP_LOGI(TAG, "connected to ap SSID:%s", APP_WIFI_SSID);
+  } else if (bits & WIFI_FAIL_BIT) {
+    ESP_LOGI(TAG, "Failed to connect to SSID:%s", APP_WIFI_SSID);
+  } else {
+    ESP_LOGE(TAG, "UNEXPECTED EVENT");
+  }
+
+  ESP_ERROR_CHECK(esp_event_handler_unregister(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler));
+  ESP_ERROR_CHECK(esp_event_handler_unregister(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler));
+  vEventGroupDelete(s_wifi_event_group);
+}
+
+static void initialize_nvs() {
+  esp_err_t err = nvs_flash_init();
+  if (err == ESP_ERR_NVS_NO_FREE_PAGES || err == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+    ESP_ERROR_CHECK(nvs_flash_erase());
+    err = nvs_flash_init();
+  }
+  ESP_ERROR_CHECK(err);
+}
+
+void app_main() {
+  // delay for console
+  vTaskDelay(1000 / portTICK_PERIOD_MS);
+
+  initialize_nvs();
+  // init wifi
+  wifi_conn_init();
+
+  printf("Hello world! ESP-IDF %s\n", esp_get_idf_version());
+
+  for (int i = 20; i >= 0; i--) {
+    printf("Restarting in %d seconds...\n", i);
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+  }
+  printf("Restarting now.\n");
+
+  fflush(stdout);
+  esp_restart();
+}
